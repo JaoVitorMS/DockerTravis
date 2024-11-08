beec0d5ef9992119893e2c5e1bc16ac12fca08b8
João Vitor <joao.vitorms1509@gmail.com>
2024-11-08 00:36:40 -0300

    feat: finish service class

---
+++ src/main/java/org/zsh/dockertravisgradle/service/ProductService.java
@@ -0,0 +1,50 @@
+package org.zsh.dockertravisgradle.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.zsh.dockertravisgradle.dto.Mapper;
+import org.zsh.dockertravisgradle.dto.ProductDto;
+import org.zsh.dockertravisgradle.model.Product;
+import org.zsh.dockertravisgradle.repository.ProductRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    private Mapper mapper;
+
+    public void salvarProduto ( ProductDto productDto ) {
+        Product product = mapper.toEntity( productDto );
+        productRepository.save(product);
+    }
+
+    public void deletarProduto ( ProductDto productDto) {
+        Product product = mapper.toEntity( productDto );
+        productRepository.delete(product);
+    }
+
+    public List<ProductDto> listAll() {
+        List<Product> products = productRepository.findAll();
+        return products.stream()
+                .map(product -> mapper.toDto( product ))
+                .toList();
+    }
+
+    public List<ProductDto> listByName ( String name ) {
+        List<Product> products = productRepository.findByName( name );
+        return products.stream()
+                .map(product -> mapper.toDto(product))
+                .toList();
+    }
+
+    public List<ProductDto> listByPrice ( Double price ) {
+        List<Product> products = productRepository.findByPrice( price );
+        return products.stream()
+                .map(product -> mapper.toDto(product))
+                .toList();
+    }
+}
